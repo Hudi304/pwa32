bf7167824dd1ed1b21c0a4f9d88d78437e0cdc20
Hudi304 <hudisteanum@yahoo.com>
2022-10-21 17:30:58 +0300

    cache-first

--- service-worker.js
+++ service-worker.js
@@ -5,8 +5,8 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox
 workbox.routing.registerRoute(
     ({request}) => request.destination === 'image',
     //? the PWA will try to read the cache first before going over the network 
-    // new workbox.strategies.CacheFirst()
+    new workbox.strategies.CacheFirst()
 
-    //? the PWA will try to read the cache first before going over the network 
-    new workbox.strategies.NetworkFirst()
+    // //? the PWA will try to read the cache first before going over the network 
+    // new workbox.strategies.NetworkFirst()
 );
