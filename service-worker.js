05e30b2cbbe9907a68caaa835224db7cfa2a3665
Hudi304 <hudisteanum@yahoo.com>
2022-10-21 16:04:25 +0300

    add call

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,12 @@
+
+//? import from CDN
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
 
 workbox.routing.registerRoute(
     ({request}) => request.destination === 'image',
-    new workbox.strategies.CacheFirst()
+    //? the PWA will try to read the cache first before going over the network 
+    // new workbox.strategies.CacheFirst()
+
+    //? the PWA will try to read the cache first before going over the network 
+    new workbox.strategies.NetworkFirst()
 );
