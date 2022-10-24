97080364785bd88a4787323d2ac4a4381f7aec39
Laurentiuu <laurgales@yahoo.com>
2022-10-24 10:44:26 +0300

    blanitz

--- sw.js
+++ sw.js
@@ -26,3 +26,10 @@ async function cacheFirst(req) {
   const cachedResponse = await cache.match(req);
   return cachedResponse || fetch(req);
 }
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
+
+workbox.routing.registerRoute(
+    ({request}) => request.destination === 'image',
+    new workbox.strategies.CacheFirst()
+);
